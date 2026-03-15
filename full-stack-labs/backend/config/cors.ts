126ad97ecadb2b4ee200a7fa2b78eb18c7f03df7
MariaMead <mmead@Academic.RRC.ca>
2026-03-14 21:36:02 -0500

    Fixed cors config to work in deployment.

--- full-stack-labs/backend/config/cors.ts
+++ full-stack-labs/backend/config/cors.ts
@@ -5,12 +5,11 @@ const corsOptions: CorsOptions = {
     // throw an error if the request does not come from the list of allowed origins
     origin: function(origin, callback) {
         console.log('Request Origin:', origin);
-        const allowedOrigins = [process.env.FRONTEND_URL,process.env.FRONTEND_URL_MAIN] 
-        ;
+        const allowedOrigins = (process.env.FRONTEND_URL ?? '')?.split(',');
         
         // invoke callback (eg. next middleware) if  origin matches or no origin
         // some services (like postman) do not include an origin in their request
-        if(allowedOrigins.includes(origin) || !origin) {
+        if(! origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS restriction"), false);
